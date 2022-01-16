b08dda22db3a247116d141dcaf6fa282b085e471
R Pesc <rich.pesc@gmail.com>
2022-01-15 22:56:13 -0500

    inital commit

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,19 +1,20 @@
-import { Box } from "@mui/material";
 import useStyles from "../styles";
+import { Container, Typography } from "@mui/material";
 
 const Dashboard = () => {
   const video = require("../media/lowPolyBackground.mp4");
   const styles = useStyles();
   return (
-    <>
-      <table>
-        <video loop autoPlay muted className={styles.bgVideo}>
-          <source src={video} type="video/mp4" />
-          Browser does not support video
-        </video>
-        <div className={styles.whiteFade} />
-      </table>
-    </>
+    <Container className={styles.whiteFade}>
+      <video loop autoPlay muted className={styles.bgVideo}>
+        <source src={video} type="video/mp4" />
+        Browser does not support video
+      </video>
+      <div className={styles.whiteFade} />
+      <Typography variant="h3" color="black">
+        Welcome
+      </Typography>
+    </Container>
   );
 };
 
